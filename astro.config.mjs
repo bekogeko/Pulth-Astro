9ca4ed635608844ac68a00e22b554a7ceb3f7ffa
Bekir Gulestan <gulestanbekir@gmail.com>
2024-04-03 06:39:47 -0500

    add robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://blog.pulth.com",
-    integrations: [tailwind(), sitemap(), react()]
+    integrations: [tailwind(), sitemap(), react(), robots()]
 });
