8832e9d0195fe606ee4edba5317ff6878648e406
Bekir Gülestan <gulestanbekir@gmail.com>
2024-10-17 18:57:56 -0500

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 import robots from "astro-robots";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blog.pulth.com",
-    integrations: [tailwind(), sitemap(), react(), robots()]
+  site: "https://blog.pulth.com",
+  integrations: [tailwind(), sitemap(), react(), robots()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
